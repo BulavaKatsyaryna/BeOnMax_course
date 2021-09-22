f80c0f0e60b60f12c9ad9d19ca1d886ae4f9834f
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-09-23 01:15:28 +0300

    Working with testing. and exception handling

--- src/main/java/JUnit/src/main/java/Calculator.java
+++ src/main/java/JUnit/src/main/java/Calculator.java
@@ -1,6 +1,12 @@
 public class Calculator {
 
-    int add(int a, int b) {
+    public double add(int a, int b) {
         return a + b;
+    }
+
+    public double add(String a, String b) {
+        int num1 = Integer.parseInt(a);
+        int num2 = Integer.parseInt(b);
+        return num1 + num2;
     }
 }
--- src/main/java/JUnit/src/main/test/CalculatorTest.java
+++ src/main/java/JUnit/src/main/test/CalculatorTest.java
@@ -1,15 +1,36 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CalculatorTest {
 
     @Test
-    void add() {
+    public void add() {
         Calculator calculator = new Calculator();
-        int expected = 15;
-        int result = calculator.add(10, 5);
-        assertEquals(expected, result);
-
+        double expected = 15;
+        double result = calculator.add(10, 5);
+        assertEquals(expected, result, 0.000001);
     }
+
+    @Test
+    public void when5AddTo10AsStringThenResult15() {
+        Calculator calculator = new Calculator();
+        double expected = 15;
+        double result = calculator.add("10", "5");
+        assertEquals(expected, result, 0.000001);
+    }
+
+    @Test
+    public void whenInputIncorrectValueThenThrowException() {
+        Calculator calculator = new Calculator();
+        boolean wasException = false;
+        try {
+            calculator.add("dhmd", "5");
+        } catch (Exception e) {
+            wasException = true;
+        }
+        assertTrue(wasException);
+    }
+
 }
