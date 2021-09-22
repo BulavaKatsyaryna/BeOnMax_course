4d9c8d8bdaaddce0bec830078a67dd22bbcf1782
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-09-23 00:30:47 +0300

    Add files via upload

---
+++ src/main/java/JUnit/src/main/java/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println("10 + 5 = " + calculator.add(10, 5));
+    }
+}
