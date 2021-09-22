cf4abcb35fac4ff8ac494b7dc74bde12055cc9f0
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-09-23 02:08:59 +0300

    Tests for the project are written

---
+++ src/main/java/CollectionFramework/src/main/java/Car.java
@@ -0,0 +1,12 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Car {
+
+    private String brand;
+    private int number;
+}
