8fb32500c75e77357c03bd7016ecb333df52cfa9
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-09-26 02:09:37 +0300

    Working with equals () and hashcode ()

--- src/main/java/CollectionFramework/src/main/java/Main.java
+++ src/main/java/CollectionFramework/src/main/java/Main.java
@@ -3,6 +3,8 @@ public class Main {
         Car car1 = new Car("BMW", 1);
         Car car2 = new Car("BMW", 1);
         System.out.println(car1.equals(car2));
+        System.out.println(car1.hashCode());
+        System.out.println(car2.hashCode());
 
     }
 }
