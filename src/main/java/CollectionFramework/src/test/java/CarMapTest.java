1705ce87b0e6845ecf9c435a207ae002f216db05
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-10-09 01:56:12 +0300

    Tests for HashMap collection are written

--- src/main/java/CollectionFramework/src/test/java/CarMapTest.java
+++ src/main/java/CollectionFramework/src/test/java/CarMapTest.java
@@ -30,4 +30,44 @@ public class CarMapTest {
         }
         assertEquals(10, map.size());
     }
+
+    @Test
+    public void removeReturnTrueOnlyOnce(){
+        for (int i = 0; i < 10; i++) {
+            CarOwner carOwner = new CarOwner(i, "Name" + 1, "LastName" + 1);
+            Car car = new Car("Brand" + 1, i);
+            map.put(carOwner, car);
+        }
+        assertEquals(10, map.size());
+
+        CarOwner elementForDeleting = new CarOwner(5, "Name5", "LastName5");
+        assertTrue(map.remove(elementForDeleting));
+        assertEquals(9, map.size());
+        assertFalse(map.remove(elementForDeleting));
+    }
+
+    @Test
+    public void countOfKeyMustBeEqualsToCountOfValues() {
+        for (int i = 0; i < 10; i++) {
+            CarOwner carOwner = new CarOwner(i, "Name" + 1, "LastName" + 1);
+            Car car = new Car("Brand" + 1, i);
+            map.put(carOwner, car);
+        }
+        assertEquals(100, map.size());
+        assertEquals(100, map.keySet().size());
+        assertEquals(100, map.values().size());
+    }
+
+    @Test
+    public void methodGetMustReturnRightValue() {
+        for (int i = 0; i < 10; i++) {
+            CarOwner carOwner = new CarOwner(i, "Name" + 1, "LastName" + 1);
+            Car car = new Car("Brand" + 1, i);
+            map.put(carOwner, car);
+        }
+        CarOwner key = new CarOwner(50, "Name50", "LastName50");
+        Car value = map.get(key);
+        String expectedCarBrand = "Brand50";
+        assertEquals(expectedCarBrand, value.getBrand());
+    }
 }
