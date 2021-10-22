5efa0a4f308b3d479923d9523a1f3efe9943474c
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-10-23 00:41:12 +0300

    The collection Queue has been examined. Implementation and tests written

--- src/main/java/CollectionFramework/src/main/java/CarLinkedList.java
+++ src/main/java/CollectionFramework/src/main/java/CarLinkedList.java
@@ -4,7 +4,7 @@ import lombok.NoArgsConstructor;
 
 import java.util.Iterator;
 
-public class CarLinkedList implements CarList {
+public class CarLinkedList implements CarList, CarQueue {
 
     private Node first;
     private Node last;
@@ -28,6 +28,18 @@ public class CarLinkedList implements CarList {
         }
         size++;
         return true;
+    }
+
+    @Override
+    public Car peek() {
+        return size > 0 ? get(0) : null;
+    }
+
+    @Override
+    public Car poll() {
+        Car car = get(0);
+        removeAt(0);
+        return car;
     }
 
     @Override
