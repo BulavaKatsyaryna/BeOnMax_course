2444ee1784f996bffc96820f4a67e2f7351178a5
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-10-25 00:44:31 +0300

    initial commit

--- src/main/java/Concurrency/src/main/java/Main.java
+++ src/main/java/Concurrency/src/main/java/Main.java
@@ -1,93 +1,64 @@
-import java.util.TreeMap;
-
 public class Main {
 
-    private static final int SIZE = 10_000_000;
-    private static final int HALF = SIZE / 2;
-
     public static void main(String[] args) {
-        startTimer();
-        withoutConcurrency();
-        withConcurrency();
-    }
 
-    private static void startTimer() {
-        Thread timer = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                int second = 0;
-                try {
-                    while (true) {
-                        System.out.println(second++);
-                        Thread.sleep(1000);
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        timer.setDaemon(true);
-        timer.start();
-    }
-
-    private static void withoutConcurrency() {
-        float[] array = new float[SIZE];
-        for (int i = 0; i < SIZE; i++) {
-            array[i] = 1f;
-        }
+        Counter counter = new Counter();
         long before = System.currentTimeMillis();
-        for (int i = 0; i < SIZE; i++) {
-            float f = (float) i;
-            array[i] = (float) (array[i] * Math.sin(0.2f + f / 5) * Math.cos(0.2f + f / 5) * Math.cos(0.4f + f / 2));
-        }
-        long after = System.currentTimeMillis();
-        System.out.println("withoutConcurrency: " + (after - before));
-    }
+        int barrier = 10_000_000;
 
-    private static void withConcurrency() {
-        float[] array = new float[SIZE];
-        for (int i = 0; i < SIZE; i++) {
-            array[i] = 1f;
-        }
-        long before = System.currentTimeMillis();
-        float[] firstHalf = new float[HALF];
-        float[] secondHalf = new float[HALF];
-        System.arraycopy(array, 0, firstHalf, 0, HALF);
-        System.arraycopy(array, HALF, secondHalf, 0, HALF);
-
-        Thread tread1 = new Thread(new Runnable() {
+        Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < HALF; i++) {
-                    float f = (float) i;
-                    firstHalf[i] = (float) (firstHalf[i] * Math.sin(0.2f + f / 5) * Math.cos(0.2f + f / 5) * Math.cos(0.4f + f / 2));
+                for (int i = 0; i < barrier; i++) {
+                    counter.inc();
                 }
             }
         });
 
-        Thread tread2 = new Thread(new Runnable() {
+        Thread thread2 = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < HALF; i++) {
-                    float f = (float) (i + HALF);
-                    secondHalf[i] = (float) (secondHalf[i] * Math.sin(0.2f + f / 5) * Math.cos(0.2f + f / 5) * Math.cos(0.4f + f / 2));
+                for (int i = 0; i < barrier; i++) {
+                    counter.dec();
                 }
             }
         });
 
-        tread1.start();
-        tread2.start();
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < barrier; i++) {
+                    counter.inc2();
+                }
+            }
+        });
+
+        Thread thread4 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < barrier; i++) {
+                    counter.dec2();
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
 
         try {
-            tread1.join();
-            tread2.join();
+            thread1.join();
+            thread2.join();
+            thread3.join();
+            thread4.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.arraycopy(firstHalf, 0, array, 0, HALF);
-        System.arraycopy(secondHalf, 0, array, HALF, HALF);
+        System.out.println(counter.getValue());
+        System.out.println(counter.getValue2());
 
         long after = System.currentTimeMillis();
-        System.out.println("withConcurrency: " + (after - before));
+        System.out.println(after - before);
     }
 }
