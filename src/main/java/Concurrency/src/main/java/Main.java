d80f12e2081327b05f6a15aba3fff81457f314ce
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-10-24 15:40:24 +0300

    Examined the sleep method. And exception handling

--- src/main/java/Concurrency/src/main/java/Main.java
+++ src/main/java/Concurrency/src/main/java/Main.java
@@ -2,9 +2,32 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Start");
-        Thread thread = new MyThread();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < 1000; i++) {
+                        if (Thread.currentThread().isInterrupted()) {
+                            break;
+                        }
+                        System.out.print(i);
+                        Thread.sleep(1000);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
         thread.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         for (int i = 0; i < 1000; i ++) {
+            if (i == 100) {
+                thread.interrupt();
+            }
             System.out.print("M");
         }
         System.out.println("\nFinish");
