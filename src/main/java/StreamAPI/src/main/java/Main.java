5488f136fad2feb961b63dfb4fdcb7ba466fabfe
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-10-26 00:43:39 +0300

    initial commit

--- src/main/java/StreamAPI/src/main/java/Main.java
+++ src/main/java/StreamAPI/src/main/java/Main.java
@@ -1,14 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-//        new Thread(() -> System.out.println(1)).start();
 
-        Director director = new Director();
-        String result = director.force((n) -> {
-            for (int i = 0; i < n; i++) {
-                System.out.println("Working...");
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            numbers.add((int) (Math.random() * 1000));
+        }
+        List<Integer> filtered = filter(numbers);
+        for (int i : filtered) {
+            System.out.println(i);
+        }
+    }
+
+    private static List<Integer> filter(List<Integer> list) {
+        List<Integer> result = new ArrayList<>();
+        for (int i : list) {
+            if (i % 2 == 0) {
+                result.add(i);
             }
-            return "Success";
-        }, 5);
-        System.out.println(result);
+        }
+        return result;
     }
 }
