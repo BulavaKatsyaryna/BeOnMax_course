06dc99bb031f218ec2b64e73a125a447e5638817
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-10-27 01:11:23 +0300

    initial commit

--- src/main/java/StreamAPI/src/main/java/Main.java
+++ src/main/java/StreamAPI/src/main/java/Main.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,11 +10,21 @@ public class Main {
         for (int i = 0; i < 100; i++) {
             numbers.add((int) (Math.random() * 1000));
         }
-        List<Integer> filtered = filter(numbers, (n) -> n % 2 == 0);
+        List<String> list = numbers.stream()
+                .filter((n) -> n % 2 ==0)
+                .map((integer -> "Number: " + integer))
+                .filter((string) -> string.endsWith("0"))
+                .map((string) -> string + "!")
+                .collect(Collectors.toList());
 
-        for (int i : filtered) {
-            System.out.println(i);
+    }
+
+    private static List<String> map(List<Integer> numbers) {
+        List<String> result = new ArrayList<>();
+        for (int number : numbers) {
+            result.add("Number: " + number);
         }
+        return result;
     }
 
     private static List<Integer> filter(List<Integer> list, Predicate predicate) {
