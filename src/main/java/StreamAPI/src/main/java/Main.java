4f2d9f5b2c0da46ac428dc7eccc1c7b925ed637c
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-10-27 02:14:25 +0300

    initial commit

--- src/main/java/StreamAPI/src/main/java/Main.java
+++ src/main/java/StreamAPI/src/main/java/Main.java
@@ -1,25 +1,21 @@
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<User> users = new ArrayList<>();
+        List<Float> numbers = new ArrayList<>();
+        for (int i = 0; i < 30_000_000; i++) {
+            numbers.add((float) i);
+        }
 
-        users.add(new User("Alex", 35));
-        users.add(new User("Max", 22));
-        users.add(new User("John", 17));
-        users.add(new User("Andrew", 33));
-        users.add(new User("Jack", 44));
-        users.add(new User("Nick", 80));
-        users.add(new User("Alice", 46));
-        users.add(new User("Helen", 11));
+        long before = System.currentTimeMillis();
 
-        users.stream()
-                .filter(user1 -> user1.getName().contains("l"))
-                .findFirst()
-                .ifPresentOrElse(System.out::println, () -> System.out.println("User not found!"));
+        numbers.stream()
+                .map((number) -> Math.sin(0.2f + number / 5) * Math.cos(0.2f + number / 5) * Math.cos(0.4f + number / 2))
+                .collect(Collectors.toList());
+
+        long after = System.currentTimeMillis();
+        System.out.println(after - before);
     }
 }
