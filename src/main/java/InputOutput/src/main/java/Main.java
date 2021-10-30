12900e03d20d4cff2170ab6b89c5b3012ff05e26
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-10-31 01:52:38 +0300

    solved the problem of reading names from a file and dividing them into lines

--- src/main/java/InputOutput/src/main/java/Main.java
+++ src/main/java/InputOutput/src/main/java/Main.java
@@ -1,24 +1,23 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         File directory = new File("folder");
-        directory.mkdir();
         File file = new File(directory, "names.txt");
-        try {
-            file.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
         try (Reader reader = new InputStreamReader(new FileInputStream(file))) {
-            int a = reader.read();
+            char[] array = new char[128];
+            int count = reader.read(array);
             StringBuilder result = new StringBuilder();
-            while (a > 0) {
-                result.append((char) a);
-                a = reader.read();
+            while (count > 0) {
+                result.append(new String(array, 0, count));
+                count = reader.read(array);
             }
-            System.out.println(result.toString());
+            String[] names = result.toString().split(" ");
+            Arrays.stream(names)
+                    .filter(name -> name.startsWith("A"))
+                    .forEach(System.out::println);
         } catch (Exception e) {
             e.printStackTrace();
         }
