30522289db2b7da78b90c60c4c880a577d07402b
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-10-31 01:09:48 +0300

    solved the problem with reading names from a file

--- src/main/java/InputOutput/src/main/java/Main.java
+++ src/main/java/InputOutput/src/main/java/Main.java
@@ -2,13 +2,14 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File("1.txt");
+        File directory = new File("folder");
+        directory.mkdir();
+        File file = new File(directory, "names.txt");
         try {
             file.createNewFile();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         try (InputStream inputStream = new FileInputStream(file)) {
             int a = inputStream.read();
             while (a != -1) {
